fbb4c0319f578a67cfdb36b89f73aeffa82d6366
Dannyang27 <danny27995@gmail.com>
2017-12-06 18:02:25 +0000

    refactored method - > Util class

--- app/src/main/java/com/example/dannyang27/bitcoinvalue/Adapters/BitcoinAdapter.java
+++ app/src/main/java/com/example/dannyang27/bitcoinvalue/Adapters/BitcoinAdapter.java
@@ -38,8 +38,7 @@ public class BitcoinAdapter extends RecyclerView.Adapter<BitcoinViewholder> {
     @Override
     public void onBindViewHolder(BitcoinViewholder holder, int position) {
 
-        getImgByName(holder, currencyList.get(position).getClass().getSimpleName());
-       // holder.img.setImageResource(Util.getImgByName(currencyList.get(position).getClass().getSimpleName()));
+        holder.img.setImageResource(Util.getImgByName(currencyList.get(position).getClass().getSimpleName()));
         holder.currency_abbr.setText(currencyList.get(position).getClass().getSimpleName());
         holder.currency_value.setText(currencyList.get(position).getLast() + currencyList.get(position).getSymbol());
         holder.currency_name.setText(Util.getFullName(currencyList.get(position).getClass().getSimpleName()));
@@ -51,54 +50,4 @@ public class BitcoinAdapter extends RecyclerView.Adapter<BitcoinViewholder> {
         return currencyList.size();
     }
 
-    public int getImgByName(BitcoinViewholder holder, String abbr){
-        int drawable = R.drawable.empty;
-        if (abbr.equals("AUD")) {
-            holder.img.setImageResource(R.drawable.aud);
-        }else if (abbr.equals("BRL")) {
-            holder.img.setImageResource(R.drawable.brl);
-        } else if (abbr.equals("CAD")) {
-            holder.img.setImageResource(R.drawable.cad);
-        } else if (abbr.equals("CHF")) {
-            holder.img.setImageResource(R.drawable.chf);
-        } else if (abbr.equals("CLP")) {
-            holder.img.setImageResource(R.drawable.clp);
-        } else if (abbr.equals("CNY")) {
-            holder.img.setImageResource(R.drawable.cny);
-        } else if (abbr.equals("DKK")) {
-            holder.img.setImageResource(R.drawable.dkk);
-        } else if (abbr.equals("EUR")) {
-            holder.img.setImageResource(R.drawable.eur);
-        } else if (abbr.equals("GBP")) {
-            holder.img.setImageResource(R.drawable.gbp);
-        } else if (abbr.equals("HKD")) {
-            holder.img.setImageResource(R.drawable.hkd);
-        } else if (abbr.equals("INR")) {
-            holder.img.setImageResource(R.drawable.inr);
-        } else if (abbr.equals("ISK")) {
-            holder.img.setImageResource(R.drawable.isk);
-        } else if (abbr.equals("JPY")) {
-            holder.img.setImageResource(R.drawable.jpy);
-        } else if (abbr.equals("KRW")) {
-            holder.img.setImageResource(R.drawable.krw);
-        } else if (abbr.equals("NZD")) {
-            holder.img.setImageResource(R.drawable.nzd);
-        } else if (abbr.equals("PLN")) {
-            holder.img.setImageResource(R.drawable.pln);
-        } else if (abbr.equals("RUB")) {
-            holder.img.setImageResource(R.drawable.rub);
-        } else if (abbr.equals("SEK")) {
-            holder.img.setImageResource(R.drawable.sek);
-        } else if (abbr.equals("SGD")) {
-            holder.img.setImageResource(R.drawable.sgd);
-        } else if (abbr.equals("THB")) {
-            holder.img.setImageResource(R.drawable.thb);
-        } else if (abbr.equals("TWD")) {
-            holder.img.setImageResource(R.drawable.twd);
-        } else if (abbr.equals("USD")) {
-            holder.img.setImageResource(R.drawable.usd);
-        }
-
-        return drawable;
-    }
 }
